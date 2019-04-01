738bc0d8d9b1c9b2e1343881d1fbad6af43101c0
Marc Tremblay <marc.tremblay1995@gmail.com>
2019-03-31 22:29:27 -0400

    new idea, pushing my work

---
+++ project2/Assets/Scripts/BoxLift.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxLift : MonoBehaviour
+{
+    public Animator anim;
+
+    public bool lifted = false;
+
+    private void Start()
+    {
+        anim.SetBool("IsUp", lifted);
+    }
+
+    public void Toggle()
+    {
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (info.IsName("BoxLifting") || info.IsName("BoxLowering")) return;
+
+        lifted = !lifted;
+        anim.SetBool("IsUp", lifted);
+    }
+}
